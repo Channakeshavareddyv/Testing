a8ccacb57afc798e9d428d2f7d665826afe670c9
CHANNAKESHVA REDDY <CHANNAKESHVA REDDY@DESKTOP-9OKQOG7>
2022-03-28 15:08:51 +0530

    Commit Create Contact

---
+++ src/test/java/com/crm/vtiger/CreateContact.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class CreateContact {
+	@Test
+	public void createContact() {
+		System.out.println("create Contact");
+	}
+
+}
